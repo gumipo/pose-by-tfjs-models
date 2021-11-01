948e2b7e9cc00b950755378908b6a2e7fe431d05
gumipo <10sei0otto.22@gmail.com>
2021-11-02 08:58:38 +0900

    add styled-components

---
+++ src/PoseNet.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as posenet from '@tensorflow-models/posenet';
+import Webcam from 'react-webcam';
+
+const PoseNet = () => {
+  return (
+    <div>
+      <h1>ダミー</h1>
+    </div>
+  );
+};
+
+export default PoseNet;
